7c3567e4b8e53e0bd22176d9c617c9fe08cd6c79
JØRDAN SANTØS <jordansantos-11@outlook.com>
2022-10-19 20:43:37 -0300

    Project 80%

---
+++ src/Component/Ods.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import * as S from '../Style/Style'
+
+export default function Ods(){
+    return(
+        <S.ContainerOds>
+             <S.GlobalStyle/>
+            <div>
+               
+                
+                    <S.H1>Os Objetivos de Desenvolvimento Sustentável em Fortaleza</S.H1>
+                    <S.H3>
+                        <h3>Os Objetivos de Desenvolvimento Sustentável são um apelo global à ação para acabar com a pobreza, proteger o meio ambiente e o clima e garantir que as pessoas, em todos os lugares, possam desfrutar de paz e de prosperidade. Estes são os objetivos para os quais as Nações Unidas estão contribuindo a fim de que possamos atingir a Agenda 2030 no Brasil.</h3>
+                    </S.H3>
+                
+                <S.Icons>
+                <S.DivOne>
+                    <div>
+                        <h2>1</h2>
+                        <p>Equidade Territorial, Social e Econômica</p>
+                    </div>
+                    <S.iconOne/>
+                    <a href='https://observatoriodefortaleza.fortaleza.ce.gov.br/relacao-de-indicadores-2040/0' target={'_blank'} rel="noopener noreferrer" >Saiba Mais</a>
+                </S.DivOne>
+                
+                <S.DivTwo>
+                    <div>
+                        <h2>2</h2>
+                        <p>Cidade conectada, Acessível e Justa</p>
+                    </div>
+                    <S.iconTwo/>
+                    <a href='https://observatoriodefortaleza.fortaleza.ce.gov.br/relacao-de-indicadores-2040/1' target={'_blank'} rel="noopener noreferrer">Saiba Mais</a>
+                </S.DivTwo>
+                <S.DivThree>
+                    <div>
+                        <h2>3</h2>
+                        <p>Vida Comunitária, Acolhimento e Bem-estar</p>
+                    </div>
+                    <S.iconThree/>
+                    <a href='https://observatoriodefortaleza.fortaleza.ce.gov.br/relacao-de-indicadores-2040/2' target={'_blank'} rel="noopener noreferrer" >Saiba Mais</a>
+                </S.DivThree>
+                <S.DivFour>
+                    <div>
+                        <h2>4</h2>
+                        <p>Desenvolvimento da Cultura e do Conhecimento</p>
+                    </div>
+                <S.iconFour/>
+                <a href='https://observatoriodefortaleza.fortaleza.ce.gov.br/relacao-de-indicadores-2040/3' target={'_blank'} rel="noopener noreferrer">Saiba Mais</a>
+                </S.DivFour>
+                <S.DivFive>
+                    <div>
+                        <h2>5</h2>
+                        <p>Qualidade do Meio Ambiente e dos Recursos Naturais</p>
+                    </div>
+                    <S.iconFive/>
+                    <a href='https://observatoriodefortaleza.fortaleza.ce.gov.br/relacao-de-indicadores-2040/4' target={'_blank'} rel="noopener noreferrer">Saiba Mais</a>
+                </S.DivFive>
+                <S.DivSix>
+                    <div>
+                        <h2>6</h2>
+                        <p>Dinamização Econômica e inclusão Produtiva</p>
+                    </div>
+                    <S.iconSix/>
+                    <a href='https://observatoriodefortaleza.fortaleza.ce.gov.br/relacao-de-indicadores-2040/5' target={'_blank'} rel="noopener noreferrer">Saiba Mais</a>
+                </S.DivSix>
+                <S.DivSeven>
+                    <div>
+                        <h2>7</h2>
+                        <p>Governança Municipal</p>
+                    </div>
+                    <S.iconSeven/>
+                    <a href='https://observatoriodefortaleza.fortaleza.ce.gov.br/relacao-de-indicadores-2040/6' target={'_blank'} rel="noopener noreferrer">Saiba Mais</a>
+                </S.DivSeven>
+                </S.Icons>
+            </div>
+        </S.ContainerOds>
+    )
+}
---
+++ src/Style/Style.jsx
@@ -0,0 +1,331 @@
+import styled, {createGlobalStyle} from "styled-components";
+import {BsFillPeopleFill} from 'react-icons/bs'
+import {GiModernCity, GiLifeTap, GiClassicalKnowledge, GiPayMoney} from 'react-icons/gi'
+import {IoEarthOutline} from 'react-icons/io5'
+import {RiGovernmentLine} from 'react-icons/ri'
+export const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+`
+
+export const ContainerOds = styled.section`
+    display: flex;
+    a{
+        text-decoration: none;
+        color: white;
+    }
+`
+
+
+export const H1 = styled.h1`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 2rem;
+    text-align: center;
+    height: 20vh;
+    @media (max-width: 768px){
+        height: 35vh;
+    }
+`
+
+export const H3 = styled.h3`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 40vh;
+    h3{
+        width: 70%;
+        font-size: 1.5rem;
+        text-align: justify;
+        
+    }
+    @media (max-width: 768px){
+        height: 80vh;
+    }
+`
+
+export const Icons = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    @media (max-width: 768px){
+        flex-direction: column;
+        align-items: center;
+        height: 300vh;
+        justify-content: space-around;
+    }
+`
+
+export const DivOne = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    background-color: #F5A836;
+    width: 20%;
+    height: 35vh;
+    border-radius: 5%;
+    div{
+        display: flex;
+        align-items: center;
+        width: 80%;
+        justify-content: space-around;
+        h2{
+            font-size: 3rem;
+            color: white;
+            width: 20%;
+        }
+        p{
+            width: 95%;
+            text-align: center;
+            font-size: 1.2rem;
+            color: white;
+            font-weight: 600;
+        }
+    }
+    @media (max-width: 768px){
+        width: 60%;
+        height: 30vh;
+    }
+`
+export const pzinho = styled.p`
+    text-align: left;
+    width: 20%;
+`
+
+export const iconOne = styled(BsFillPeopleFill)`
+    font-size: 4rem;
+    color:white;
+`
+
+
+
+export const DivTwo = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    background-color: #E2672D;
+    width: 20%;
+    height: 35vh;
+    border-radius: 5%;
+    div{
+        display: flex;
+        align-items: center;
+        width: 80%;
+        justify-content: space-around;
+        h2{
+            font-size: 3rem;
+            color: white;
+            width: 20%;
+        }
+        p{
+            width: 95%;
+            text-align: center;
+            font-size: 1.2rem;
+            color: white;
+            font-weight: 600;
+        }
+    }
+    @media (max-width: 768px){
+        width: 60%;
+        height: 30vh;
+    }
+`
+export const iconTwo = styled(GiModernCity)`
+    font-size: 4rem;
+    color: white;
+`
+
+export const DivThree = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    background-color: #F3696D;
+    width: 20%;
+    height: 35vh;
+    border-radius: 5%;
+    div{
+        display: flex;
+        align-items: center;
+        width: 80%;
+        justify-content: space-around;
+        h2{
+            font-size: 3rem;
+            color: white;
+            width: 20%;
+        }
+        p{
+            width: 95%;
+            text-align: center;
+            font-size: 1.2rem;
+            color: white;
+            font-weight: 600;
+        }
+    }
+    @media (max-width: 768px){
+        width: 60%;
+        height: 30vh;
+    }
+`
+
+export const iconThree = styled(GiLifeTap)`
+    font-size: 4rem;
+    color: white;
+`
+
+export const DivFour = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    background-color: #AB6AA3;
+    width: 20%;
+    height: 35vh;
+    border-radius: 5%;
+    div{
+        display: flex;
+        align-items: center;
+        width: 80%;
+        justify-content: space-around;
+        h2{
+            font-size: 3rem;
+            color: white;
+            width: 20%;
+        }
+        p{
+            width: 95%;
+            text-align: center;
+            font-size: 1.2rem;
+            color: white;
+            font-weight: 600;
+        }
+    }
+    @media (max-width: 768px){
+        width: 60%;
+        height: 30vh;
+    }
+`
+
+export const iconFour = styled(GiClassicalKnowledge)`
+    font-size: 4rem;
+    color: white;
+`
+
+export const DivFive = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    background-color: #4AAD8D;
+    width: 20%;
+    height: 35vh;
+    border-radius: 5%;
+    div{
+        display: flex;
+        align-items: center;
+        width: 80%;
+        justify-content: space-around;
+        h2{
+            font-size: 3rem;
+            color: white;
+            width: 20%;
+        }
+        p{
+            width: 95%;
+            text-align: center;
+            font-size: 1.2rem;
+            color: white;
+            font-weight: 600;
+        }
+    }
+    @media (max-width: 768px){
+        width: 60%;
+        height: 30vh;
+    }
+`
+
+export const iconFive = styled(IoEarthOutline)`
+    font-size: 4rem;
+    color: white;
+`
+
+export const DivSix = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    background-color: #43B4D4;
+    width: 20%;
+    height: 35vh;
+    border-radius: 5%;
+    div{
+        display: flex;
+        align-items: center;
+        width: 80%;
+        justify-content: space-around;
+        h2{
+            font-size: 3rem;
+            color: white;
+            width: 20%;
+        }
+        p{
+            width: 95%;
+            text-align: center;
+            font-size: 1.2rem;
+            color: white;
+            font-weight: 600;
+        }
+    }
+    @media (max-width: 768px){
+        width: 60%;
+        height: 30vh;
+    }
+`
+
+export const iconSix = styled(GiPayMoney)`
+    font-size: 4rem;
+    color: white;
+`
+
+export const DivSeven = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    background-color: #3D61A9;
+    width: 20%;
+    height: 35vh;
+    border-radius: 5%;
+    div{
+        display: flex;
+        align-items: center;
+        width: 80%;
+        justify-content: space-around;
+        h2{
+            font-size: 3rem;
+            color: white;
+            width: 20%;
+        }
+        p{
+            width: 95%;
+            text-align: center;
+            font-size: 1.2rem;
+            color: white;
+            font-weight: 600;
+        }
+    }
+    @media (max-width: 768px){
+        width: 60%;
+        height: 30vh;
+    }
+`
+
+export const iconSeven = styled(RiGovernmentLine)`
+    font-size: 4rem;
+    color: white;
+`
